5e35b92bb5fb63f71ab623ca6b822e94ff82db61
ahydorn <adamhydorn@gmail.com>
2019-06-04 09:56:34 -0700

    started adding 7 guesses rule

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,6 +9,7 @@ let chosenWord = word[randNum];
 let rightWord = [];
 let wrongWord = [];
 let underscore = [];
+let numberGuesses = 7;
 
 //  Dom manip
 let docUnderscore = document.getElementsByClassName('underscore');
@@ -26,25 +27,41 @@ let generateUnderscore = () => {
 //  Listen for guess
 document.addEventListener('keypress', (event) => {
     let keyword = String.fromCharCode(event.keyCode);
+
     //  If user guess is right
     if (chosenWord.indexOf(keyword) > -1) {
+
         //  Add to right words array
         rightWord.push(keyword);
+
         //  Replace underscore with correct letter after guess
         underscore[chosenWord.indexOf(keyword)] = keyword;
         docUnderscore[0].innerHTML = underscore.join(' ');
         docRightGuess[0].innerHTML = rightWord;
 
         //  Does user's word match?
-        if (underscore.join('') == chosenWord) {
+        if (underscore.join('') === chosenWord) {
             alert('Winner!');
         }
     } else {
         wrongWord.push(keyword);
         docWrongGuess[0].innerHTML = wrongWord;
     }
+
+    // Set initial number of guesses to 7
+    for (let j = 7; j >= 0; j--) {
+        // When guesses === 0, reset game
+        console.log(j);
+        if (j === 0) {
+            alert("You lose!");
+        }
+    }
+
 });
 
+// TODO //
+// Only give players 7 guesses
+// End game after 7 guesses
 
 
-docUnderscore[0].innerHTML = generateUnderscore().join(' ');
+docUnderscore[0].innerHTML = generateUnderscore().join('_');
